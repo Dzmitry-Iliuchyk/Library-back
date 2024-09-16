b2bf78a080588d844608eba977cde43e2a4a5acb
Dmitry.Iliuchyk <dima6061551@gmail.com>
2024-09-16 14:26:23 +0300

    Auth start

---
+++ Library.Infrastracture/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Infrastracture {
+    public class JwtOptions {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Secret { get; set; }
+        public int ExpiresMinutes { get; set; }
+    }
+}
